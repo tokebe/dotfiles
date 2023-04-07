fa60c0b7aed1863745fa31067be3a656e4449d20
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-07 12:34:18 -0400

    formatting

--- nvim/lua/plugins/codewindow.lua
+++ nvim/lua/plugins/codewindow.lua
@@ -1,12 +1,12 @@
---return {}
 return {
   'gorbit99/codewindow.nvim',
+  event = 'BufEnter',
   config = function()
     local codewindow = require('codewindow')
     codewindow.setup({
-        auto_enable = true,
-        minimap_width = 10,
-        window_border = "none",
+      auto_enable = true,
+      minimap_width = 10,
+      window_border = "none",
     })
     codewindow.apply_default_keybinds()
   end
