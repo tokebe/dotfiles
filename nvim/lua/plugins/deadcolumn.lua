3d0be8e1ee20f25cea130d2ad0c9631e4317da95
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-07 12:34:41 -0400

    lazier loading

--- nvim/lua/plugins/deadcolumn.lua
+++ nvim/lua/plugins/deadcolumn.lua
@@ -1,6 +1,7 @@
 -- Provides an animated column showing the max line length
 return {
   'Bekaboo/deadcolumn.nvim',
+  event = 'BufEnter',
   opts = {
     warning = {
       hlgroup = {
--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,8 +1,7 @@
 return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
-  -- Enable `lukas-reineke/indent-blankline.nvim`
-  -- See `:help indent_blankline.txt`
+  event = 'BufEnter',
   config = function()
     require('indent_blankline').setup({
       char = '│',
