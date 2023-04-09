5e8eb6753e80295d08929a62525e627382ce8ddc
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-09 10:59:29 -0400

    formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,16 @@
---[[ 
-This config is loosely based on https://github.com/nvim-lua/kickstart.nvim 
+--[[
+This config is loosely based on https://github.com/nvim-lua/kickstart.nvim
 Thanks to them!
 
-The main changes are that this structure seeks to even further break 
-configuration into multiple files, for the sake of separation of concerns. 
+The main changes are that this structure seeks to even further break
+configuration into multiple files, for the sake of separation of concerns.
 Configs for things will only be grouped if it makes sense to do so.
 
 Wish me luck.
 --]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are required 
+--  NOTE: Must happen before plugins are required
 -- (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
