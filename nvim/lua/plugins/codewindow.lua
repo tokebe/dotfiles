43cbf52f546c3f9e3a7d1c9da44f14206755e3a9
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-09 10:57:19 -0400

    don't lazy to BufEnter

--- nvim/lua/plugins/codewindow.lua
+++ nvim/lua/plugins/codewindow.lua
@@ -1,11 +1,15 @@
 return {
   'gorbit99/codewindow.nvim',
-  event = 'BufEnter',
+  dependencies = {
+    { 'nvim-treesitter/nvim-treesitter' }, -- syntax highlight
+    { 'neovim/nvim-lspconfig' } -- error highlight
+  },
   config = function()
     local codewindow = require('codewindow')
     codewindow.setup({
       auto_enable = true,
       minimap_width = 10,
+      width_multiplier = 4,
       window_border = "none",
     })
     codewindow.apply_default_keybinds()
