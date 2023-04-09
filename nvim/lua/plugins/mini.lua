92ad042e23e211893f21dc43f1049159d5a64144
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-09 10:58:21 -0400

    don't pair directly before character

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -8,6 +8,15 @@ return {
     require('mini.pairs').setup({
       mappings = {
         [' '] = { action = 'open', pair = '  ', neigh_pattern = '[{(%[][%])}]' }, -- in-bracket spacing
+        ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\]%s' },
+        ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\]%s' },
+        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\]%s' },
+        [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\]%s' },
+        [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\]%s' },
+        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\]%s' },
+        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\]%s', register = { cr = false } },
+        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\]%s', register = { cr = false } },
+        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\]%s', register = { cr = false } },
       }
     })
     require('mini.basics').setup({
