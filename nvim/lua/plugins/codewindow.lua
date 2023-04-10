9e37dd3ab4199b555c8ca9307cb7d5799d057456
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-10 00:00:29 -0400

    formatting, control hint

--- nvim/lua/plugins/codewindow.lua
+++ nvim/lua/plugins/codewindow.lua
@@ -1,16 +1,24 @@
 return {
+         -- File minimap
   'gorbit99/codewindow.nvim',
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter' }, -- syntax highlight
-    { 'neovim/nvim-lspconfig' } -- error highlight
+    { 'neovim/nvim-lspconfig' }            -- error highlight
   },
   config = function()
     local codewindow = require('codewindow')
     codewindow.setup({
+                       -- TODO don't appear in cmdline and such
       auto_enable = true,
       minimap_width = 10,
       width_multiplier = 4,
       window_border = "none",
+    })
+    -- Set up which-key hint
+    require('which-key').register({
+      ['<leader>m'] = {
+        name = "Minimap..."
+      }
     })
     codewindow.apply_default_keybinds()
   end
