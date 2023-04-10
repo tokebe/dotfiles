75e37c9d595e841ea144e3dda8bcd57e53c0efff
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-10 00:02:42 -0400

    better nested pairs, i think

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -8,15 +8,15 @@ return {
     require('mini.pairs').setup({
       mappings = {
         [' '] = { action = 'open', pair = '  ', neigh_pattern = '[{(%[][%])}]' }, -- in-bracket spacing
-        ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\]%s' },
-        ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\]%s' },
-        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\]%s' },
-        [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\]%s' },
-        [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\]%s' },
-        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\]%s' },
-        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\]%s', register = { cr = false } },
-        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\]%s', register = { cr = false } },
-        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\]%s', register = { cr = false } },
+        ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\][%s%{%(%[%]%)%}]' },
+        ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\][%s%{%(%[%]%)%}]' },
+        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\][%s%{%(%[%]%)%}]' },
+        [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\][%s%{%(%[%]%)%}]' },
+        [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\][%s%{%(%[%]%)%}]' },
+        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\][%s%{%(%[%]%)%}]' },
+        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
+        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
+        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
       }
     })
     require('mini.basics').setup({
@@ -34,6 +34,12 @@ return {
       autocommands = {
         basic = true,
         relnum_in_visual_mode = true,
+      }
+    })
+    -- Set up which-key hint
+    require('which-key').register({
+      ['<leader>t'] = {
+        name = 'Toggle...'
       }
     })
   end
