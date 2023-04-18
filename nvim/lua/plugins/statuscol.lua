b03dab28f38a8e4642f689695cdf5fc3a01f851e
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-18 18:17:37 -0400

    update+config statuscol

--- nvim/lua/plugins/statuscol.lua
+++ nvim/lua/plugins/statuscol.lua
@@ -1,13 +1,21 @@
 return {
   'luukvbaal/statuscol.nvim',
+  pin = true,
   config = function()
     local builtin = require('statuscol.builtin')
     require('statuscol').setup({
       relculright = true,
       segments = {
-        { text = { '%s' }, click = 'v:lua.ScSa' },
-        { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
-        { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+        {
+          sign = { name = { "Diagnostic" }, maxwidth = 2, colwidth = 1, auto = false },
+          click = "v:lua.ScSa"
+        },
+        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+        {
+          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true },
+          click = "v:lua.ScSa"
+        },
       },
     })
   end,
