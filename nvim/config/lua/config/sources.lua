63a8354d99d9a5c5fefb199842c8801c6dc81050
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-24 13:03:51 -0400

    optional reinstall nvim

--- nvim/config/lua/config/sources.lua
+++ nvim/config/lua/config/sources.lua
@@ -1,22 +1,26 @@
 return {
-  lsp = { -- Language Server Protocol
-    'lua_ls', -- Lua
+  lsp = {       -- Language Server Protocol
+    'lua_ls',   -- Lua
     'tsserver', -- Typescript/Javascript
-    'pyright', -- Python
-    'taplo', -- TOML
+    'pyright',  -- Python
+    'taplo',    -- TOML
+    'bashls',
   },
   dap = { -- Debug Adapter Protocol
     'node-debug2-adapter',
-    'debugpy'
+    'debugpy',
+    'bash-debug-adapter',
   },
   formatter = {
     -- formatters
     'sytlua',
     'prettier',
     'black',
+    'beautysh',
     -- linters
     'luacheck',
     'eslint_d',
     'flake8',
+    'shellharden',
   },
 }
