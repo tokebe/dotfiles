8602a190bb2a77ec29879dfe588ecd44cc031b33
tokebe <43009413+tokebe@users.noreply.github.com>
2023-05-24 21:51:26 -0400

    config navbuddy

--- nvim/lua/config/nvim_navbuddy.lua
+++ nvim/lua/config/nvim_navbuddy.lua
@@ -1,11 +1,42 @@
 return {
   config = function()
     require('nvim-navbuddy').setup({
-      border = 'none',
+      window = {
+        border = 'none',
+      },
       size = '80%',
       lsp = {
         auto_attach = true,
-      }
+        preference = require('config.sources').lsp,
+      },
+      icons = {
+        File = ' ',
+        Module = ' ',
+        Namespace = ' ',
+        Package = '󰏓 ',
+        Class = ' ',
+        Method = ' ',
+        Property = ' ',
+        Field = ' ',
+        Constructor = ' ',
+        Enum = ' ',
+        Interface = ' ',
+        Function = '󰊕 ',
+        Variable = ' ',
+        Constant = ' ',
+        String = ' ',
+        Number = ' ',
+        Boolean = ' ',
+        Array = ' ',
+        Object = '  ',
+        Key = '󰌆 ',
+        Null = 'ﳠ ',
+        EnumMember = ' ',
+        Struct = ' ',
+        Event = ' ',
+        Operator = ' ',
+        TypeParameter = ' ',
+      },
     })
-  end
+  end,
 }
