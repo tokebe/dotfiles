f0fc5e8547c398680bcf76a8a4df24e939341f63
tokebe <43009413+tokebe@users.noreply.github.com>
2023-09-06 11:54:11 -0400

    new breadcrumbs (pending nvim 0.10.0 stable)

--- nvim/lua/ui/breadcrumbs.lua
+++ nvim/lua/ui/breadcrumbs.lua
@@ -1,35 +1,45 @@
 return {
-  'utilyre/barbecue.nvim',
-  name = 'barbecue',
-  version = '*',
-  dependencies = {
-    'SmiteshP/nvim-navic',
-    'nvim-tree/nvim-web-devicons',
+  {
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
+    dependencies = {
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      -- triggers CursorHold event faster
+      vim.opt.updatetime = 200
+
+      require('barbecue').setup({
+        create_autocmd = false, -- prevent barbecue from updating itself automatically
+        attach_navic = false, -- prevent barbecu from attaching automatically
+      })
+
+      vim.api.nvim_create_autocmd({
+        'WinScrolled', -- or WinResized on NVIM-v0.9 and higher
+        'BufWinEnter',
+        'CursorHold',
+        'InsertLeave',
+
+        -- include these if you have set `show_modified` to `true`
+        'BufWritePost',
+        'TextChanged',
+        'TextChangedI',
+      }, {
+        group = vim.api.nvim_create_augroup('barbecue.updater', {}),
+        callback = function()
+          require('barbecue.ui').update()
+        end,
+      })
+    end,
   },
-  config = function()
-    -- triggers CursorHold event faster
-    vim.opt.updatetime = 200
-
-    require('barbecue').setup({
-      create_autocmd = false, -- prevent barbecue from updating itself automatically
-      attach_navic = false, -- prevent barbecu from attaching automatically
-    })
-
-    vim.api.nvim_create_autocmd({
-      'WinScrolled', -- or WinResized on NVIM-v0.9 and higher
-      'BufWinEnter',
-      'CursorHold',
-      'InsertLeave',
-
-      -- include these if you have set `show_modified` to `true`
-      'BufWritePost',
-      'TextChanged',
-      'TextChangedI',
-    }, {
-      group = vim.api.nvim_create_augroup('barbecue.updater', {}),
-      callback = function()
-        require('barbecue.ui').update()
-      end,
-    })
-  end,
+  -- { -- To be enabled when latest stable neovim supports it
+  --   'Bekaboo/dropbar.nvim',
+  --   config = function ()
+  --     require('dropbar').setup({
+  --       update_interval = 1
+  --     })
+  --   end
+  -- }
 }
