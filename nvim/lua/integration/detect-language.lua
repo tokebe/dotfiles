cff5b6105cfad4008d0e0bde250bf6efbe2292b4
tokebe <43009413+tokebe@users.noreply.github.com>
2024-01-24 17:50:34 -0500

    only detect language on file read

--- nvim/lua/integration/detect-language.lua
+++ nvim/lua/integration/detect-language.lua
@@ -1,9 +1,13 @@
 return {
   'spywhere/detect-language.nvim',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter'
+    'nvim-treesitter/nvim-treesitter',
   },
-  config = function ()
-    require('detect-language').setup()
-  end
+  config = function()
+    local util = require('util')
+    require('detect-language').setup({
+      events = { 'FileReadPost' },
+    })
+    util.keymap('n', '<Leader>dl', '<CMD>DetectLanguage<CR>', { desc = 'Detect Langauge' })
+  end,
 }
