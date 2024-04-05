84b0b70c83f294e491d138d7f9d087fdef8f6b41
tokebe <43009413+tokebe@users.noreply.github.com>
2024-04-05 17:03:07 -0400

    indent-blankline -> hlchunk

--- nvim/lua/ui/indent-guide.lua
+++ nvim/lua/ui/indent-guide.lua
@@ -1,24 +1,53 @@
 return {
-  -- Add indentation guides even on blank lines
-  'lukas-reineke/indent-blankline.nvim',
-  event = 'BufEnter',
-  config = function()
-    local highlight = {
-      'CursorColumn',
-      'Whitespace',
-    }
-    require('ibl').setup({
-      indent = {
-        char = '│',
-        -- char = '',
-        -- highlight = highlight,
-      },
-      whitespace = {
-        -- highlight = highlight,
-      },
-      exclude = {
-        filetypes = require('config.filetype_excludes'),
-      },
-    })
-  end,
+  -- {
+  --   -- Add indentation guides even on blank lines
+  --   'lukas-reineke/indent-blankline.nvim',
+  --   event = 'BufEnter',
+  --   config = function()
+  --     local highlight = {
+  --       'CursorColumn',
+  --       'Whitespace',
+  --     }
+  --     require('ibl').setup({
+  --       indent = {
+  --         char = '│',
+  --         -- char = '',
+  --         -- highlight = highlight,
+  --       },
+  --       whitespace = {
+  --         -- highlight = highlight,
+  --       },
+  --       exclude = {
+  --         filetypes = require('config.filetype_excludes'),
+  --       },
+  --     })
+  --   end,
+  -- },
+  {
+    'shellRaining/hlchunk.nvim',
+    event = { 'UIEnter' },
+    config = function()
+      require('hlchunk').setup({
+        chunk = {
+          chars = {
+            right_arrow = '─',
+          },
+          use_treesitter = false,
+        },
+        indent = {
+          enable = true,
+          chars = { '│', '┊' }
+        },
+        line_num = {
+          enable = true,
+        },
+        blank = {
+          enable = false,
+          chars = {
+            '░', ' '
+          }
+        },
+      })
+    end,
+  },
 }
