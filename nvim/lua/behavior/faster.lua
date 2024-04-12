8e2790eabc9da1bdf637ea54600b4e99d3dbd55b
tokebe <43009413+tokebe@users.noreply.github.com>
2024-04-12 12:26:05 -0400

    disable faster until later config

--- nvim/lua/behavior/faster.lua
+++ nvim/lua/behavior/faster.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    'pteroctopus/faster.nvim',
-    config = function ()
-      require('faster').setup()
-    end
-  }
+--   {
+--     'pteroctopus/faster.nvim',
+--     config = function ()
+--       require('faster').setup()
+--     end
+--   }
 }
