8bb3d638edf45a1709decf88526ffc49c4a238b6
tokebe <43009413+tokebe@users.noreply.github.com>
2024-06-06 09:45:50 -0700

    disable specs (buggy)

--- nvim/lua/ui/animations.lua
+++ nvim/lua/ui/animations.lua
@@ -1,28 +1,28 @@
 return {
-  {
-    -- Animate cursor jumping larger distances
-    'edluffy/specs.nvim',
-    event = 'BufEnter',
-    config = function()
-      require('specs').setup({
-        show_jumps = true,
-        min_jump = 5,
-        popup = {
-          delay_ms = 0, -- delay before popup displays
-          inc_ms = 10, -- time increments used for fade/resize effects
-          blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
-          width = 10,
-          winhl = 'Search',
-          fader = require('specs').pulse_fader,
-          resizer = require('specs').shrink_resizer,
-        },
-        ignore_filetypes = {},
-        ignore_buftypes = {
-          nofile = true,
-        },
-      })
-    end,
-  },
+  -- {
+  --   -- Animate cursor jumping larger distances
+  --   'edluffy/specs.nvim',
+  --   event = 'BufEnter',
+  --   config = function()
+  --     require('specs').setup({
+  --       show_jumps = true,
+  --       min_jump = 5,
+  --       popup = {
+  --         delay_ms = 0, -- delay before popup displays
+  --         inc_ms = 10, -- time increments used for fade/resize effects
+  --         blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
+  --         width = 10,
+  --         winhl = 'Search',
+  --         fader = require('specs').pulse_fader,
+  --         resizer = require('specs').shrink_resizer,
+  --       },
+  --       ignore_filetypes = {},
+  --       ignore_buftypes = {
+  --         nofile = true,
+  --       },
+  --     })
+  --   end,
+  -- },
   { -- Smoothly move cursor
     'gen740/SmoothCursor.nvim',
     event = 'BufEnter',
