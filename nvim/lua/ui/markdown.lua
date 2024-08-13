df913c9d61a204e9d5a0e1bf254ae0b5d0fb09f7
tokebe <43009413+tokebe@users.noreply.github.com>
2024-08-13 18:12:30 -0400

    switch to markview

--- nvim/lua/ui/markdown.lua
+++ nvim/lua/ui/markdown.lua
@@ -1,10 +1,20 @@
 return {
-  'MeanderingProgrammer/markdown.nvim',
-  name = 'render-markdown',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-  },
-  config = function()
-    require('render-markdown').setup()
-  end,
+  -- {
+  --   'MeanderingProgrammer/markdown.nvim',
+  --   name = 'render-markdown',
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  --   config = function()
+  --     require('render-markdown').setup()
+  --   end,
+  -- },
+  {
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+    }
+  }
 }
