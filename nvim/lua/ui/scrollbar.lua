60069ef882eb5c62bc1a282aec1e4940d115a642
tokebe <43009413+tokebe@users.noreply.github.com>
2024-08-19 14:02:13 -0400

    switch to satellite

--- nvim/lua/ui/scrollbar.lua
+++ nvim/lua/ui/scrollbar.lua
@@ -1,27 +1,55 @@
 return {
-  -- {
-  --   'lewis6991/satellite.nvim',
-  --   config = function()
-  --     require('satellite').setup({})
-  --   end,
-  -- },
-  -- {
-  --   'dstein64/nvim-scrollview',
-  --   config = function()
-  --     require('scrollview').setup({
-  --       excluded_filetypes = { 'nerdtree' },
-  --       current_only = true,
-  --       winblend = 50,
-  --     })
-  --   end,
-  -- },
   {
-    'petertriho/nvim-scrollbar',
+    'lewis6991/satellite.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'lewis6991/gitsigns.nvim' },
     config = function()
-      require('scrollbar').setup({
-        show_in_active_only = true,
+      require('satellite').setup({
+        current_only = true,
         excluded_filetypes = require('config.filetype_excludes'),
       })
     end,
   },
+  -- {
+  --   'petertriho/nvim-scrollbar',
+  --   config = function()
+  --     require('scrollbar').setup({
+  --       show_in_active_only = true,
+  --       excluded_filetypes = require('config.filetype_excludes'),
+  --     })
+  --   end,
+  -- },
+  -- Not very friendly with wrap on
+  -- {
+  --   'Isrothy/neominimap.nvim',
+  --   lazy = false,
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'lewis6991/gitsigns.nvim' },
+  --   keys = {
+  --     {
+  --       '<Leader>tm',
+  --       '<CMD>Neominimap toggle<CR>',
+  --       desc = 'Toggle minimap',
+  --     },
+  --     {
+  --       'M',
+  --       '<CMD>Neominimap toggleFocus<CR>',
+  --       desc = 'Focus minimap (fast scroll)',
+  --     },
+  --   },
+  --   config = function()
+  --     vim.opt.sidescrolloff = 10
+  --     vim.g.neominimap = {
+  --       auto_enable = true,
+  --       exclue_filestypes = require('config.filetype_excludes'),
+  --       minimap_width = 10,
+  --       click = {
+  --         enabled = true,
+  --         auto_switch_focus = false,
+  --       },
+  --       search = {
+  --         enabled = true,
+  --       },
+  --       window_border = 'none',
+  --     }
+  --   end,
+  -- },
 }
