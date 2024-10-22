401f85630b8266041e472b21ad3dd8675bfed735
tokebe <43009413+tokebe@users.noreply.github.com>
2024-10-22 17:23:10 -0400

    switch back to markdown-render

--- nvim/lua/ui/markdown.lua
+++ nvim/lua/ui/markdown.lua
@@ -1,20 +1,43 @@
 return {
-  -- {
-  --   'MeanderingProgrammer/markdown.nvim',
-  --   name = 'render-markdown',
-  --   dependencies = {
-  --     'nvim-treesitter/nvim-treesitter',
-  --   },
-  --   config = function()
-  --     require('render-markdown').setup()
-  --   end,
-  -- },
   {
-    'OXY2DEV/markview.nvim',
-    lazy = false,
+    'MeanderingProgrammer/markdown.nvim',
+    name = 'render-markdown',
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons'
-    }
-  }
+      'nvim-tree/nvim-web-devicons',
+    },
+    events = {
+      'FileType'
+    },
+    config = function()
+      require('render-markdown').setup({
+        file_types = {
+          'markdown',
+          'quarto',
+          'rmd',
+        },
+        render_modes = { 'n', 'c' },
+        heading = {
+          width = 'block',
+          min_width = 88,
+          border = true,
+        },
+        code = {
+          position = 'right',
+          language_pad = 1,
+          width = 'block',
+          min_width = 88,
+          border = 'thick',
+        },
+      })
+    end,
+  },
+  -- {
+  --   'OXY2DEV/markview.nvim',
+  --   lazy = false,
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-tree/nvim-web-devicons'
+  --   }
+  -- }
 }
