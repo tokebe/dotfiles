fc9bea4df958ca1d3660ff7b4edadbd06c852206
tokebe <43009413+tokebe@users.noreply.github.com>
2024-10-22 17:57:46 -0400

    add csv view

---
+++ nvim/lua/integration/language-support/csv.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'hat0uma/csvview.nvim',
+    config = function()
+      local csv = require('csvview')
+      require('csvview').setup({
+        view = {
+          display_mode = 'highlight',
+        },
+      })
+      vim.keymap.set('n', '<Leader>tc', function()
+        csv.toggle()
+        vim.opt.wrap = not vim.opt.wrap
+      end, { desc = 'Toggle csv view' })
+    end,
+  },
+}
