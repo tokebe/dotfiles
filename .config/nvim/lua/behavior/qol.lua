afd52d956844d5bc735e861b4286a4e171530b8e
tokebe <43009413+tokebe@users.noreply.github.com>
2024-12-13 11:57:05 -0500

    updated tmux integration

--- .config/nvim/lua/behavior/qol.lua
+++ .config/nvim/lua/behavior/qol.lua
@@ -166,9 +166,20 @@ return {
   { -- Automatic intelligent tab size detection
     'tpope/vim-sleuth',
   },
-  {
-    'christoomey/vim-tmux-navigator',
-    lazy = false,
+  { -- TMUX movement integration
+    'alexghergh/nvim-tmux-navigation',
+    config = function()
+      require('nvim-tmux-navigation').setup({
+        keybindings = {
+          left = '<C-h>',
+          down = '<C-j>',
+          up = '<C-k>',
+          right = '<C-l>',
+          last_active = '<C-\\>',
+          next = '<C-Space>',
+        },
+      })
+    end,
   },
   {
     'chrisgrieser/nvim-early-retirement',
