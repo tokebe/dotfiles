ebc84588efebddd2e1dd9267c98b39e274515e31
tokebe <43009413+tokebe@users.noreply.github.com>
2025-01-15 16:29:38 -0500

    small fix

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -63,7 +63,7 @@ config.tab_bar_style = {
 -- It prefers the title that was set via `tab:set_title()`
 -- or `wezterm cli set-tab-title`, but falls back to the
 -- title of the active pane in that tab.
-function tab_title(tab_info)
+local function tab_title(tab_info)
   local title = tab_info.tab_title
   -- if the tab title is explicitly set, take that
   if title and #title > 0 then
@@ -74,7 +74,7 @@ function tab_title(tab_info)
   return tab_info.active_pane.title
 end
 
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+wezterm.on('format-tab-title', function(tab, tabs, _, _, _, max_width)
   local fg = colorscheme_table.selection_bg
   local bg = colorscheme_table.ansi[1]
   local text = colorscheme_table.foreground
