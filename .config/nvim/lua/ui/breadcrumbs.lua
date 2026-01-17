90e695e2760885cd804f94bafc2e323c19979165
tokebe <43009413+tokebe@users.noreply.github.com>
2026-01-16 19:46:44 -0500

    disable breadcrumb preview

--- .config/nvim/lua/ui/breadcrumbs.lua
+++ .config/nvim/lua/ui/breadcrumbs.lua
@@ -1,47 +1,13 @@
 return {
-  -- {
-  --   'utilyre/barbecue.nvim',
-  --   name = 'barbecue',
-  --   version = '*',
-  --   dependencies = {
-  --     'SmiteshP/nvim-navic',
-  --     'nvim-tree/nvim-web-devicons',
-  --   },
-  --   config = function()
-  --     -- triggers CursorHold event faster
-  --     vim.opt.updatetime = 200
-  --
-  --     require('barbecue').setup({
-  --       create_autocmd = false, -- prevent barbecue from updating itself automatically
-  --       attach_navic = false, -- prevent barbecu from attaching automatically
-  --     })
-  --
-  --     vim.api.nvim_create_autocmd({
-  --       'WinScrolled', -- or WinResized on NVIM-v0.9 and higher
-  --       'BufWinEnter',
-  --       'CursorHold',
-  --       'InsertLeave',
-  --
-  --       -- include these if you have set `show_modified` to `true`
-  --       'BufWritePost',
-  --       'TextChanged',
-  --       'TextChangedI',
-  --     }, {
-  --       group = vim.api.nvim_create_augroup('barbecue.updater', {}),
-  --       callback = function()
-  --         require('barbecue.ui').update()
-  --       end,
-  --     })
-  --   end,
-  -- },
+  -- ,
   { -- To be enabled when latest stable neovim supports it
     'Bekaboo/dropbar.nvim',
-    config = function ()
+    config = function()
       require('dropbar').setup({
-        bar = {
-          update_debounce = 1
-        }
+        menu = {
+          preview = false,
+        },
       })
-    end
-  }
+    end,
+  },
 }
