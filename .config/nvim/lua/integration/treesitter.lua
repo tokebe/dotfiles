b6d9ad421b26c16b49127d4e1d26f37a3d4e0ebc
tokebe <43009413+tokebe@users.noreply.github.com>
2026-02-17 17:59:52 -0500

    nvim: fix search repeats

--- .config/nvim/lua/integration/treesitter.lua
+++ .config/nvim/lua/integration/treesitter.lua
@@ -166,8 +166,8 @@ return {
 
       -- Repeat movement with ; and ,
       -- ensure ; goes forward and , goes backward regardless of the last direction
-      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next)
-      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous)
+      -- vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move_next)
+      -- vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_previous)
 
       -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
       vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f_expr, { expr = true })
