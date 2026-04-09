71900a9e69cc0f6144606fc4894283acbe4897cc
tokebe <43009413+tokebe@users.noreply.github.com>
2026-04-09 11:15:40 -0700

    better edit discovery autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -12,10 +12,13 @@ vim.api.nvim_create_autocmd('ModeChanged', {
   desc = 'Prevent luasnip from jumping back after finishing a snippet',
 })
 
-vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI', 'BufEnter', 'FocusGained' }, {
+vim.api.nvim_create_autocmd('FocusGained', {
+  desc = 'Reload files from disk when we focus vim',
   pattern = '*',
-  callback = function()
-    vim.cmd('checktime')
-  end,
-  desc = 'Check for file changes periodically',
+  command = "if getcmdwintype() == '' | checktime | endif",
+})
+vim.api.nvim_create_autocmd('BufEnter', {
+  desc = 'Every time we enter an unmodified buffer, check if it changed on disk',
+  pattern = '*',
+  command = "if &buftype == '' && !&modified && expand('%') != '' | exec 'checktime ' . expand('<abuf>') | endif",
 })
